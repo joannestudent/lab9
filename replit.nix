38902af7643b192d8a32ea956ed77925756889b0
joannethacker <jo.taormina@gmail.com>
2026-04-28 17:06:33 -0400

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,6 +1,8 @@
 { pkgs }: {
-  deps = [
-    pkgs.jdk17
-    pkgs.maven
-  ];
+    deps = [
+        pkgs.graalvm17-ce
+        pkgs.maven
+        pkgs.replitPackages.jdt-language-server
+        pkgs.replitPackages.java-debug
+    ];
 }
